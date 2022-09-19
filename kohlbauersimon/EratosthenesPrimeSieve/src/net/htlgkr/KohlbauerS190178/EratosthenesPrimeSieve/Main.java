032609f030f8d8e89c0936cd8beb9af71444c324
kohlbauers190178 <113886098+kohlbauers190178@users.noreply.github.com>
2022-09-19 11:25:09 +0200

    Delete kohlbauersimon/EratosthenesPrimeSieve directory

--- kohlbauersimon/EratosthenesPrimeSieve/src/net/htlgkr/KohlbauerS190178/EratosthenesPrimeSieve/Main.java
+++
@@ -1,8 +0,0 @@
-package net.htlgkr.KohlbauerS190178.EratosthenesPrimeSieve;
-
-public class Main {
-    public static void main(String[] args){
-        EratosthenesPrimeSieve eratosthenesPrimeSieve = new EratosthenesPrimeSieve(100);
-        eratosthenesPrimeSieve.printPrimes();
-    }
-}
