47f2de29509bfd93b1d83b5ce17d86000888b057
kohlbauers190178 <kohlbauers190178@sus.htl-grieskirchen.at>
2022-09-19 11:30:58 +0200

    2nd commit

--- kohlbauersimon/EratosthenesPrimeSieve/src/net/htlgkr/KohlbauerS190178/EratosthenesPrimeSieve/EratosthenesPrimeSieve.java
+++ kohlbauersimon/EratosthenesPrimeSieve/src/net/htlgkr/KohlbauerS190178/EratosthenesPrimeSieve/EratosthenesPrimeSieve.java
@@ -10,12 +10,24 @@ public class EratosthenesPrimeSieve {
 
     public boolean isPrime(int number) {
 
-
+        if(number<=1)
+        {
+            return false;
+        }
+        for(int i=2;i<=number/2;i++)
+        {
+            if((number%i)==0)
+                return  false;
+        }
         return true;
     }
 
     public void printPrimes(){
-
+        for(int i=0;i<upperLimit;i++){
+            if(isPrime(i)){
+                System.out.println(i+" is a prime");
+            }
+        }
     }
 
 }
--- kohlbauersimon/EratosthenesPrimeSieve/src/net/htlgkr/KohlbauerS190178/EratosthenesPrimeSieve/Main.java
+++ kohlbauersimon/EratosthenesPrimeSieve/src/net/htlgkr/KohlbauerS190178/EratosthenesPrimeSieve/Main.java
@@ -2,6 +2,7 @@ package net.htlgkr.KohlbauerS190178.EratosthenesPrimeSieve;
 
 public class Main {
     public static void main(String[] args){
-
+        EratosthenesPrimeSieve eratosthenesPrimeSieve = new EratosthenesPrimeSieve(100);
+        eratosthenesPrimeSieve.printPrimes();
     }
 }
