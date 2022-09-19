e12aad710625cbc0545b7de4b0761ae5f62607d8
kohlbauers190178 <kohlbauers190178@sus.htl-grieskirchen.at>
2022-09-19 11:22:29 +0200

    initial commit

---
+++ kohlbauersimon/EratosthenesPrimeSieve/src/net/htlgkr/KohlbauerS190178/EratosthenesPrimeSieve/Main.java
@@ -0,0 +1,8 @@
+package net.htlgkr.KohlbauerS190178.EratosthenesPrimeSieve;
+
+public class Main {
+    public static void main(String[] args){
+        EratosthenesPrimeSieve eratosthenesPrimeSieve = new EratosthenesPrimeSieve(100);
+        eratosthenesPrimeSieve.printPrimes();
+    }
+}
